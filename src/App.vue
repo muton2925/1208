e0fa83aa54d35ff47d80633088f50573dda84443
age68573 <s1410523045@gms.nutc.edu.tw>
2021-12-08 15:31:47 +0800

    '1208v1'

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <Nav></Nav>
 </template>
-
+<script>
+import Nav from './components/global/nav.vue'
+export default {
+  components: {
+    Nav 
+  }
+}
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+h1 {
+  margin-bottom: 0 !important;
+  line-height: 1.6rem !important;
 }
 </style>
---
+++ src/components/global/nav.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="header">
+    <div class="nav_logo">
+      <img src="../../assets/free5gmano_icon.png" width="70" height="70" alt="logo_icon"/>
+      <h1 class="nav_logo_h1">FREE 5G MANO</h1>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+.header {
+  min-height: 100vh;
+  background-color: #4e73df;
+  background-image: linear-gradient(180deg, #4e73df 10%, #224abe 100%);
+  background-size: cover;
+}
+.nav_logo {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #FFF;
+  cursor: pointer;
+}
+.nav_logo_h1 {
+    display: none;
+    text-align: center;
+    letter-spacing: 0.05em;
+    font-weight: 900;
+    font-size: 16px;
+    margin-left: 4px;
+    margin-right: 4px;
+}
+@media (min-width: 576px) {
+  .header {
+    width: 104px;
+  }
+}
+@media (min-width: 768px) {
+  .header {
+    width: 224px;
+  }
+  .nav_logo {
+    padding: 0 16px;
+  }
+  .nav_logo_h1 {
+      display: inline;
+  }
+}
+</style>

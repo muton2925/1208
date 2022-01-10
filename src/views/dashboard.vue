693fa3e4236159f1e86ab7bfbcd8fd6f765c2e52
muton2925 <muton2925@gmail.com>
2022-01-11 02:31:55 +0800

    0111 v3

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -1,8 +1,19 @@
 <template>
-  <div>
+  <div class="dashboard-container">
+    <audio controls loops>
+      <source src="../assets/Rubia.mp3">
+        Your browser does not support the
+        <code>audio</code> element.
+    </audio>
   </div>
 </template>
 <script>
 </script>
 <style>
+.dashboard-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh - 70px);
+}
 </style>
